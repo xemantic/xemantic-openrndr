bc2c70f314c1fff7e785389f067be1fa51cf039c
Kazik Pogoda <morisil@xemantic.com>
2019-09-05 11:01:29 +0200

    ColorBufferRecorder moved to the top level file

--- xemantic-openrndr-video/src/main/kotlin/ColorBufferRecorder.kt
+++ xemantic-openrndr-video/src/main/kotlin/ColorBufferRecorder.kt
@@ -18,7 +18,7 @@ import java.lang.IllegalStateException
  *
  * @see org.openrndr.ffmpeg.ScreenRecorder
  * @param buffer the buffer to take video frames from.
- * @param suffix might be useful for simulations recording of many buffers
+ * @param suffix might be useful for simultaneous recording of many buffers
  *                of the same program.
  */
 class ColorBufferRecorder(
@@ -27,10 +27,10 @@ class ColorBufferRecorder(
     val preset: Preset = Preset.HIGH_QUALITY,
     val suffix: String? = null,
     val profile: VideoWriterProfile = when(preset) {
-      Preset.REAL_TIME     -> newRealTimeVideoWriterProfile()
-      Preset.HIGH_QUALITY  -> newHighQualityVideoWriterProfile()
+      Preset.REAL_TIME -> newRealTimeVideoWriterProfile()
+      Preset.HIGH_QUALITY -> newHighQualityVideoWriterProfile()
       Preset.DEMO_FESTIVAL -> newHighQualityVideoWriterProfile(frameRate = 25.0)
-      Preset.INSTAGRAM     -> newInstagramVideoWriterProfile()
+      Preset.INSTAGRAM -> newInstagramVideoWriterProfile()
     },
     val inputFrameRate: Int = 60
 ) : Extension {
@@ -104,3 +104,66 @@ class ColorBufferRecorder(
 
 }
 
+fun newRealTimeVideoWriterProfile(
+    crf: Int = 10, // 0 would mean lossless, 10 seems like a reasonable value
+    // to prevent blocky artifacts on gradients, ffmpeg default is 23
+    frameRate: Double = 60.0
+) : VideoWriterProfile {
+  return object : VideoWriterProfile() {
+    override fun arguments(): Array<String> {
+      return arrayOf(
+          "-vcodec", "libx264",
+          "-pix_fmt", "yuv420p",
+          "-an",
+          "-crf", "$crf",
+          "-r", "$frameRate",
+          "-preset", "ultrafast"
+      )
+    }
+  }
+}
+
+fun newHighQualityVideoWriterProfile(
+    crf: Int = 10, // 0 would mean lossless, 10 seems like a reasonable value
+    // to prevent blocky artifacts on gradients, ffmpeg default is 23
+    frameRate: Double = 60.0
+) : VideoWriterProfile {
+  return object : VideoWriterProfile() {
+    override fun arguments(): Array<String> {
+      return arrayOf(
+          "-vcodec", "libx264",
+          "-pix_fmt", "yuv420p",
+          "-an",
+          "-crf", "$crf",
+          "-r", "$frameRate",
+          "-preset", "slow",
+          "-tune", "film",
+          "-movflags", "+faststart"
+      )
+    }
+  }
+}
+
+fun newInstagramVideoWriterProfile(
+    crf: Int = 20, // seems that maybe it should go really up for instagram?
+    frameRate: Double = 30000.0 / 1001.0
+) : VideoWriterProfile {
+  return object : VideoWriterProfile() {
+    override fun arguments(): Array<String> {
+      return arrayOf(
+          "-vcodec", "libx264",
+          "-pix_fmt", "yuv420p",
+          "-an",
+          "-crf", "$crf",
+          "-r", "$frameRate",
+          "-preset", "veryslow",
+          "-tune", "film",
+          "-level", "4.0",
+          "-color_primaries", "1",
+          "-color_trc", "1",
+          "-colorspace", "1",
+          "-movflags", "+faststart"
+      )
+    }
+  }
+}
