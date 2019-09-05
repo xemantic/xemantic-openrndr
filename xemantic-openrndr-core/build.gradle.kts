549ea99415d02374ed071cb9cc3c9cb9c994e803
Kazik Pogoda <morisil@xemantic.com>
2019-09-05 09:30:58 +0200

    project configuration files fix

--- xemantic-openrndr-core/build.gradle.kts
+++ xemantic-openrndr-core/build.gradle.kts
@@ -3,6 +3,6 @@ dependencies {
   compile("org.openrndr:openrndr-core:${rootProject.extra["openrndrVersion"]}")
   compile("org.slf4j:slf4j-api:${rootProject.extra["slf4jVersion"]}")
   compile("io.github.microutils:kotlin-logging:${rootProject.extra["kotlinLoggingVersion"]}")
-  runtime("org.slf4j:jul-to-slf4j:${rootProject.extra["slf4jVersion"]}")
+  compile("org.slf4j:jul-to-slf4j:${rootProject.extra["slf4jVersion"]}")
   runtime("ch.qos.logback:logback-classic:${rootProject.extra["logbackVersion"]}")
 }
