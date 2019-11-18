5ba0930db538f29e746ad3e03ae74a34ffd1383b
Kazik Pogoda <morisil@xemantic.com>
2019-11-18 12:15:12 +0100

    dependency update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-extra["openrndrVersion"] = "0.3.35"
-extra["slf4jVersion"] = "1.7.26"
+extra["openrndrVersion"] = "0.3.36-rc7"
+extra["slf4jVersion"] = "1.7.29"
 extra["logbackVersion"] = "1.2.3"
-extra["kotlinLoggingVersion"] = "1.7.2"
+extra["kotlinLoggingVersion"] = "1.7.8"
 
 plugins {
   id("nebula.kotlin") version "1.3.50" apply false
